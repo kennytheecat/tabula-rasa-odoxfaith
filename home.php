f95de74f51fb093553aedc19d13dbd38d86abf24
Kenny Scott <kennytheecat@gmail.com>
2014-07-25 20:49:01 -0700

    Made homepage modules responsive

--- home.php
+++ home.php
@@ -17,11 +17,11 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<div class="featured">
-			<div class="featured-content" class="left">
-				<iframe width="400" height="300" src="http://www.youtube.com/embed/laHcgdE55Mo?rel=0" frameborder="0" allowfullscreen></iframe>
-			</div>
 			<div class="featured-text" class="left">
 			The Orthodox Church is evangelical, but not Protestant.<br />It is orthodox, but not Jewish.<br />It is catholic, but not Roman.<br />It isn't non-denominational - it is pre-denominational.<br />It has believed, taught, preserved, defended and died for the Faith of the Apostles since the Day of Pentecost 2000 years ago.
+			</div>		
+			<div class="featured-content" class="left">
+				<iframe width="400" height="300" src="http://www.youtube.com/embed/laHcgdE55Mo?rel=0" frameborder="0" allowfullscreen></iframe>
 			</div>
 		</div>		
 
@@ -36,23 +36,25 @@ get_header(); ?>
 		<?php 
 		while (have_posts()) : the_post(); 
 		?>
-		<div class="featured-cat">
-			<div class="home-featured-image">
-				<a href="<?php the_permalink() ?>">
-				<?php the_post_thumbnail('featured_cat'); ?>
-				</a>
+		<div class="featured-cat-wrapper">
+			<div class="featured-cat">
+				<div class="home-featured-image">
+					<a href="<?php the_permalink() ?>">
+					<?php the_post_thumbnail('featured_cat'); ?>
+					</a>
+				</div>
+				<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+				<div class="home-featured-text">
+					<?php the_excerpt(); ?>
+				</div>
+				<span class="featured-cross"></span>
+				<div class="read-more">
+					<a href="<?php the_permalink() ?>">Read More...</a>
+				</div>
 			</div>
-			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-			<div class="home-featured-text">
-				<?php the_excerpt(); ?>
-			</div>
-			<span class="featured-cross"></span>
-			<div class="read-more h4">
-				<a href="<?php the_permalink() ?>">Read More...</a>
-			</div>
-		</div>
-					<span class="shadow-left"></span>
+			<span class="shadow-left"></span>
 			<span class="shadow-right"></span>
+		</div>
 		<?php 
 		endwhile;
 		?>			
